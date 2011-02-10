797ce24c521b956a74ebb7d5affb9147bf9d9faf
Josh Weinberg <daemoncollector@gmail.com>
2011-02-09 18:58:54 -0600

    Finally works

--- heath.cpp
+++ heath.cpp
@@ -13,16 +13,18 @@ using namespace std;
 
 int main()
 {
-	double alpha = 1.0;
-	double a = 0.001; //.mm
-	double h = 1; //.minutes
-	int n = 10;
-	int n_timesteps = 100;
+	double alpha = 0.001;
+	double a = 0.1; //.mm
+	double h = .01; //.minutes
+	int n = 11;
+	int n_timesteps = 100000
+	;
 	vector<double> u(n);
 	vector<double> v(n);
 	for (int i = 0; i < n; ++i)
-		u[i] = (i==0)?200:0;
-	
+	{
+		v[i] = u[i] = (i==0)?200:0;
+	}
 	for (int t = 0; t < n_timesteps; ++t)
 	{
 		// u(x,t+h) = alpha*h/a^2 (u(x+a,t)-2u(x,t)+u(x-a,t)) + u(x,t)
@@ -32,8 +34,8 @@ int main()
 		{
 			v[i] = alpha*h/(a*a)*(u[i+1]-2.0*u[i]+u[i-1]) + u[i];
 		}
-		for (int i = 1; i < n-1; ++i)
-			u[i] = v[i];
+		
+		u = v;
 		printf("%d) middle T=%f\n", t, u[n/2]);
 	}
 		
